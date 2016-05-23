3b8162f2ce169945ce9a3aec6c49b8a733ea518a
Carlos Borges <caborges3@gmail.com>
2016-05-23 10:38:00 -0700

    initial commit here we go

---
+++ config/routes.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+var showsController = require('../controllers/entries');
+
+router.route('/api/entries')
+  .get(entriesController.index)
+  .post(entriesController.create);
+
+router.route('/api/entries/:id')
+  .get(entriesController.show)
+  .put(entriesController.update)
+  .delete(entriesController.destroy);
+
+/* GET home page. */
+router.get('*', function(req, res, next) {
+  res.sendFile('public/index.html');
+});
+
+module.exports = router;

9e392cabca7018cc57f3ece988d47b32d44677fb
Carlos Borges <caborges3@gmail.com>
2016-05-25 17:57:35 -0700

    posts populated into cards

--- config/seeds.js
+++ config/seeds.js
@@ -7,17 +7,22 @@ var entries = [
   {
     image: "https://cdn0.vox-cdn.com/thumbor/ZsrmegSpGQhksEJ4sZvkC_yQP1g=/1400x0/filters:no_upscale()/cdn0.vox-cdn.com/uploads/chorus_asset/file/6504799/Screen_Shot_2016-05-18_at_1.19.55_PM.0.png",
     description: "Google Home",
-    votes: "33"
+    votes: 33
   },
   {
     image: "http://www.samsung.com/us/explore/galaxy-s7-features-and-specs/dist/assets/img/module3/s7edge.jpg",
     description: "Samsung Galaxy S7 Edge",
-    votes: "7"
+    votes: 7
   },
   {
     image: "https://i1.wp.com/hypebeast.com/image/2016/05/New-Adidas-Yeezy-750-Boost-colorway-00001.jpg?w=1382",
     description: "Adidas Yeezy 750",
-    votes: "51"
+    votes: 51
+  }
+  {
+    image: "http://www.consumerreports.org/content/dam/cro/news_articles/cars/CR-Cars-II-2018-Tesla-Model-3-silver-pr-4-16.jpg",
+    description: "Tesla Model 3",
+    votes: 3
   }
 ];
 
